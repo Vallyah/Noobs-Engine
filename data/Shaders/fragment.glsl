d57619dda2102465abb3c9e5510d6eac57f9c219
Vallyah <bombay31@hotmail.fr>
2019-11-15 19:53:46 +0100

    Created pntriangles tesselation shaders

--- data/Shaders/fragment.glsl
+++ data/Shaders/fragment.glsl
@@ -1,14 +1,10 @@
 in vec3 world_pos;
-in vec3 normal;
-in vec2 uv;
-in vec3 tangent;
-in vec3 bitan;
+in vec3 te_normal;
 
 uniform vec3 ambient;
 uniform vec3 diffuse;
 uniform vec3 specular;
 uniform float shininess;
-uniform bool hasTexture;
 
 uniform sampler2D texture_diffuse1;
 uniform sampler2D texture_normal1;
@@ -21,21 +17,13 @@ out vec4 fragColor;
 
 void main()
 {
-    vec3 Ka, Kd, Ks, frag_norm;
-    if (hasTexture)
-    {
-        Kd = texture(texture_diffuse1, uv).xyz;
-        //frag_norm = texture(texture_normal1, uv).xyz;
-        frag_norm = normalize(normal);
-    }
-    else
-    {
-        Kd = diffuse;
-        Ks = specular;
-        Ka = ambient;
-        frag_norm = normalize(normal);
-    }
+    vec3 Kd = diffuse;
+    vec3 Ks = specular;
+    vec3 Ka = ambient;
+    vec3 frag_norm = normalize(te_normal);
     vec3 viewDir = normalize(viewPos - world_pos);
-    vec3 result = CalcDirLight(dirLight, frag_norm, viewDir, Ka, Kd, Ks, shininess);
-    fragColor = vec4(result, 1.0f);
+    
+    vec3 color = CalcDirLight(dirLight, frag_norm, viewDir, Ka, Kd, Ks, shininess);
+    
+    fragColor = vec4(color, 1.0f);
 }
--- data/Shaders/vertex.glsl
+++ data/Shaders/vertex.glsl
@@ -1,25 +1,9 @@
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 inormal;
-layout (location = 2) in vec2 iuv;
-layout (location = 3) in vec3 itan;
-layout (location = 4) in vec3 ibitan;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-
-out vec3 world_pos;
-out vec3 normal;
-out vec2 uv;
-out vec3 tangent;
-out vec3 bitan;
+out vec3 v_normal;
 
 void main()
 {
-    gl_Position = projection * view * model * vec4(position, 1.0f);
-    world_pos = (model * vec4(position, 1.0f)).xyz;
-    normal = inormal;
-    uv = iuv;
-    tangent = itan;
-    bitan = ibitan;
+    v_normal = inormal;
 }
