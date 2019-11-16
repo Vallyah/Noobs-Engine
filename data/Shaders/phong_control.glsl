92af88036cbc816ecee31a0ec0b28fd39ac70ce7
Vallyah <bombay31@hotmail.fr>
2019-11-16 22:27:51 +0100

    Implemented Phong tesselation

--- data/Shaders/phong_control.glsl
+++ data/Shaders/phong_control.glsl
@@ -1,24 +1,3 @@
-/* Compute control points */
-
-// As the control shader will be called 3 times (one for each vertex)
-// we work on one composant of the coeficients each time.
-struct Coefs {
-    /* Geometry */
-    float b210;
-    float b120;
-    float b021;
-    float b012;
-    float b102;
-    float b201;
-    float b111;
-    // b003, b030, b300 are already known
-    /* Normal */
-    float n110;
-    float n011;
-    float n101;
-    // n002, n020, n200 are already known
-};
-
 uniform float lod;
 
 layout(vertices=3) out;
@@ -26,58 +5,28 @@ layout(vertices=3) out;
 in vec3 v_normal[];
 
 out vec3 tc_normal[3];
-out Coefs tc_coefs[3];
+out vec3 pi_sum[3];
 
-float wij(int i, int j)
+vec3 pi(int i, vec3 q)
 {
-    return dot(gl_in[j].gl_Position.xyz - gl_in[i].gl_Position.xyz, v_normal[i]);
-}
-
-float vij(int i, int j)
-{
-    vec3 vec_PiPj = gl_in[j].gl_Position.xyz - gl_in[i].gl_Position.xyz;
-    float num = dot(vec_PiPj, v_normal[i] + v_normal[j]);
-    return 2.0f * num / dot(vec_PiPj, vec_PiPj);
+    vec3 Pi = gl_in[i].gl_Position.xyz;
+    vec3 Ni = v_normal[i];
+    return (q - dot(q - Pi, Ni) * Ni);
 }
 
 void main()
 {
-    // b003, b030, b300
+    // Pi, Pj and Pk
     gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
-    // n002, n020, n200
+    // Ni, Nj and Nk
     tc_normal[gl_InvocationID] = v_normal[gl_InvocationID];
-    
-    // known data
-    float P0 = gl_in[0].gl_Position[gl_InvocationID];
-    float P1 = gl_in[1].gl_Position[gl_InvocationID];
-    float P2 = gl_in[2].gl_Position[gl_InvocationID];
-    float N0 = v_normal[0][gl_InvocationID];
-    float N1 = v_normal[1][gl_InvocationID];
-    float N2 = v_normal[2][gl_InvocationID];
-    
+
+    int next = (gl_InvocationID + 1) % 3;
+
     // compute coefs
-    /* Geometry */
-    tc_coefs[gl_InvocationID].b210 = ((2.0f * P0 + P1) - wij(0,1) * N0) / 3.0f;
-    tc_coefs[gl_InvocationID].b120 = ((2.0f * P1 + P0) - wij(1,0) * N1) / 3.0f;
-    tc_coefs[gl_InvocationID].b021 = ((2.0f * P1 + P2) - wij(1,2) * N1) / 3.0f;
-    tc_coefs[gl_InvocationID].b012 = ((2.0f * P2 + P1) - wij(2,1) * N2) / 3.0f;
-    tc_coefs[gl_InvocationID].b102 = ((2.0f * P2 + P0) - wij(2,0) * N2) / 3.0f;
-    tc_coefs[gl_InvocationID].b201 = ((2.0f * P0 + P2) - wij(0,2) * N0) / 3.0f;
-    
-    float E = (tc_coefs[gl_InvocationID].b012 + tc_coefs[gl_InvocationID].b021 +
-               tc_coefs[gl_InvocationID].b102 + tc_coefs[gl_InvocationID].b120 +
-               tc_coefs[gl_InvocationID].b201 + tc_coefs[gl_InvocationID].b210) / 6.0f;
-    float V = (P0 + P1 + P2) / 3.0f;
-    
-    tc_coefs[gl_InvocationID].b111 = E + (E - V) / 2.0f;
-    
-    /* Normal */
-    // We can't normalize yet because it's only one composant of the vector
-    // Therefore, we postpone it to the evaluation shader.
-    tc_coefs[gl_InvocationID].n110 = N0 + N1 - vij(0,1) * (P1 - P0);
-    tc_coefs[gl_InvocationID].n011 = N1 + N2 - vij(1,2) * (P2 - P1);
-    tc_coefs[gl_InvocationID].n101 = N2 + N0 - vij(2,0) * (P0 - P2);
-    
+    pi_sum[gl_InvocationID] = pi(gl_InvocationID, gl_in[next].gl_Position.xyz) +
+                              pi(next, gl_in[gl_InvocationID].gl_Position.xyz);
+
     // set tesselation levels (~ level of detail)
     gl_TessLevelOuter[gl_InvocationID] = lod + 1.0f;
     gl_TessLevelInner[0] = lod + 1.0f;
--- data/Shaders/phong_evaluation.glsl
+++ data/Shaders/phong_evaluation.glsl
@@ -18,48 +18,36 @@ uniform mat4 projection;
 layout(triangles, equal_spacing, ccw) in;
 
 in vec3 tc_normal[];
-in Coefs tc_coefs[];
+in vec3 pi_sum[];
 
 out vec3 te_normal;
 out vec3 te_world_pos;
 
 void main()
 {
-    vec3 b300 = gl_in[0].gl_Position.xyz;
-    vec3 b030 = gl_in[1].gl_Position.xyz;
-    vec3 b003 = gl_in[2].gl_Position.xyz;
-    vec3 n200 = tc_normal[0];
-    vec3 n020 = tc_normal[1];
-    vec3 n002 = tc_normal[2];
     vec3 uvw =  gl_TessCoord;
     vec3 uvw2 = uvw * uvw; // square
-    vec3 uvw3 = uvw2 * uvw; // cube
     
-    // geometry coefficients
-    vec3 b210 = vec3(tc_coefs[0].b210, tc_coefs[1].b210, tc_coefs[2].b210);
-    vec3 b120 = vec3(tc_coefs[0].b120, tc_coefs[1].b120, tc_coefs[2].b120);
-    vec3 b021 = vec3(tc_coefs[0].b021, tc_coefs[1].b021, tc_coefs[2].b021);
-    vec3 b012 = vec3(tc_coefs[0].b012, tc_coefs[1].b012, tc_coefs[2].b012);
-    vec3 b102 = vec3(tc_coefs[0].b102, tc_coefs[1].b102, tc_coefs[2].b102);
-    vec3 b201 = vec3(tc_coefs[0].b201, tc_coefs[1].b201, tc_coefs[2].b201);
-    vec3 b111 = vec3(tc_coefs[0].b111, tc_coefs[1].b111, tc_coefs[2].b111);
+    vec3 Pi = gl_in[0].gl_Position.xyz;
+    vec3 Pj = gl_in[1].gl_Position.xyz;
+    vec3 Pk = gl_in[2].gl_Position.xyz;
     
-    // normal coefficients
-    vec3 n110 = normalize(vec3(tc_coefs[0].n110, tc_coefs[1].n110, tc_coefs[2].n110));
-    vec3 n011 = normalize(vec3(tc_coefs[0].n011, tc_coefs[1].n011, tc_coefs[2].n011));
-    vec3 n101 = normalize(vec3(tc_coefs[0].n101, tc_coefs[1].n101, tc_coefs[2].n101));
+    float alpha = 0.75f;
     
     // compute normal
-    te_normal = n200 * uvw2[2] + n020 * uvw2[0] + n002 * uvw2[1] +
-                n110 * uvw[2] * uvw[0] + n011 * uvw[0] * uvw[1] + n101 * uvw[2] * uvw[1];
+    te_normal = normalize(uvw[0] * tc_normal[0] +
+                          uvw[1] * tc_normal[1] +
+                          uvw[2] * tc_normal[2]);
+
+    vec3 bar_pos = Pi * uvw[0] + Pj * uvw[1] + Pk * uvw[2];
+
+    // compute pos
+    vec3 puv = uvw2[0] * Pi + uvw2[1] * Pj + uvw2[2] * Pk +
+               uvw[0] * uvw[1] * pi_sum[0] + uvw[1] * uvw[2] * pi_sum[1] +
+               uvw[2] * uvw[0] * pi_sum[2];
     
-    // compute position
-    uvw2 *= 3.0f;
-    vec3 buvw = b300 * uvw3[2] + b030 * uvw3[0] + b003 * uvw3[1] +
-                b210 * uvw2[2] * uvw[0] + b120 * uvw2[0] * uvw[2] +
-                b201 * uvw2[2] * uvw[1] + b021 * uvw2[0] * uvw[1] +
-                b102 * uvw2[1] * uvw[2] + b012 * uvw2[1] * uvw[0] +
-                6.0f * b111 * uvw[0] * uvw[1] * uvw[2];
-    gl_Position = projection * view * vec4(buvw,1.0);
-    te_world_pos = (vec4(buvw, 1.0f)).xyz;
+    vec3 final_pos = (1 - alpha) * bar_pos + alpha * puv;
+
+    gl_Position = projection * view * vec4(final_pos, 1.0f);
+    te_world_pos = final_pos;
 }
